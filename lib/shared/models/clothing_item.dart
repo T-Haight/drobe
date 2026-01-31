5de19949a01783d65b170a8e21b7f83471004e26
T-Haight <tomjhaight@gmail.com>
2026-01-31 18:33:16 -0500

    Implemented basic ClothingItem class and initializer.

--- lib/shared/models/clothing_item.dart
+++ lib/shared/models/clothing_item.dart
@@ -1,7 +1,32 @@
 class ClothingItem {
   final String id;
-  final String type;
-  final String color;
+  final ClothingType type;
+  final ClothingColor color;
   final List<String> tags;
   final String imageUrl;
+  final DateTime createdAt;
+
+  const ClothingItem({
+    required this.id,
+    required this.type,
+    required this.color,
+    required this.tags,
+    required this.imageUrl,
+    required this.createdAt,
+  });
+
+  factory ClothingItem.fromJson(Map<String, dynamic> json) {
+    return ClothingItem(
+      id: json['id'],
+      type: ClothingType.values.firstWhere((e) => e.name == json['type']),
+      color: ClothingColor.values.firstWhere((e) => e.name == json['color']),
+      tags: List<String>.from(json['tags']),
+      imageUrl: json['image_url'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
 }
+
+enum ClothingType { shirt, pants, jacket, shoes }
+
+enum ClothingColor { black, white, blue, red, green }
